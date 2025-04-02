ab4565f8c4a0a7e43a13a4a88f71d2fefcce17fa
Nick Olson <naolson.1819@gmail.com>
2025-04-02 15:57:30 -0500

    added get books by price endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 import { Hono } from 'hono'
+import { getBookValidator } from './schemas/get-book-schema';
+import { getBooksByPrice } from './db/queries';
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get('/books', getBookValidator, (c) => {
+  const { minPrice, maxPrice } = c.req.valid('query');
+
+  const books = getBooksByPrice(minPrice, maxPrice);
+
+  return c.json(books);
+});
 
 export default app
